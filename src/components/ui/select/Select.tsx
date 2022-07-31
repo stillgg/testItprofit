9527c2ab4def4058734433b6eb02aa2c6eee61d5
Egor <still2095@gmail.com>
2022-07-31 23:38:35 +0300

    main

--- src/components/ui/select/Select.tsx
+++ src/components/ui/select/Select.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import classes from "./Select.module.scss";
 
 export const Select = ({ ...props }) => {
-  return (
-    <select className={classes.select} {...props}>
-      {props.children}
-    </select>
-  );
+  return <select {...props}>{props.children}</select>;
 };
